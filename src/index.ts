0074ca41298679bf60aa4ec635740630bd0b17ea
Tajnymag <tajnymag@gmail.com>
2018-11-04 15:49:03 +0100

    Moving to pageAction

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import hello from './helper';
+import Tab = browser.tabs.Tab;
 
-const btnEl = document.querySelector("button");
-
-if (btnEl) {
-  btnEl.addEventListener("click", hello);
-  console.log('Test');
+function protocolIsApplicable(url: string | undefined) {
+  return url === 'https://www.uptodate.com/home';
 }
+
+function initializePageAction(tab: Tab) {
+  if (!tab.id) {
+    throw 'Tab.id or Tab.url was undefined!';
+  }
+
+  if (protocolIsApplicable(tab.url)) {
+    browser.pageAction.setIcon({tabId: tab.id, path: "assets/img/logo.svg"});
+    browser.pageAction.show(tab.id);
+  }
+}
+
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  initializePageAction(tab);
+});
